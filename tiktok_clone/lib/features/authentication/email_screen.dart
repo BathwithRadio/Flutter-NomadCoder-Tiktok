170ab51186e977885e70595a05713f07653c1656
유민기 <mgyou@nextree.io>
2023-02-09 21:16:54 +0800

    theme change

---
+++ tiktok_clone/lib/features/authentication/email_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class EmailScreen extends StatelessWidget {
+  const EmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          "Sign up",
+          style: TextStyle(
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- tiktok_clone/lib/features/authentication/widgets/auth_botton.dart
+++ tiktok_clone/lib/features/authentication/widgets/auth_botton.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/username_screen.dart';
 
 class AuthBotton extends StatelessWidget {
   final String text;
@@ -11,36 +12,46 @@ class AuthBotton extends StatelessWidget {
     required this.icon,
   });
 
+  void onUsernameScreen(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const UsernameScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: Container(
-        padding: const EdgeInsets.all(
-          Sizes.size14,
-        ),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.grey.shade200,
-            width: Sizes.size1,
+    return GestureDetector(
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Container(
+          padding: const EdgeInsets.all(
+            Sizes.size14,
           ),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: icon,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey.shade200,
+              width: Sizes.size1,
             ),
-            Text(
-              text,
-              style: const TextStyle(
-                fontSize: Sizes.size16,
-                fontWeight: FontWeight.w600,
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: icon,
               ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
