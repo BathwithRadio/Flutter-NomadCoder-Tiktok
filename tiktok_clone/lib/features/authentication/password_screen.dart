82dd75f8fc4eeb520186eadef6f008fc06ef9b04
유민기 <mgyou@nextree.io>
2023-02-13 22:06:42 +0800

    Password Screen, how to make awson vailidation

--- tiktok_clone/lib/features/authentication/password_screen.dart
+++ tiktok_clone/lib/features/authentication/password_screen.dart
@@ -1,12 +1,187 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/birthday_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-class PasswordScreen extends StatelessWidget {
+class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
 
   @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  // Controller - allow us control widget from code, method
+  final TextEditingController _passwordController = TextEditingController();
+
+  String _password = "";
+
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    //
+    super.initState(); // state initialize - so this coma first
+    _passwordController.addListener(() {
+      setState(() {
+        _password = _passwordController.text;
+      });
+    });
+  }
+
+  // 반드시 dispose 할 것 - 하지 않으면 언젠가 앱이 메모리 부족으로 크래시남
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose(); // after all - clean up - dispose at latest
+  }
+
+  bool _isPasswordValid() {
+    //
+    return _password.isNotEmpty && _password.length > 8;
+  }
+
+  // 키보드 비활성화 - Text Field가 아닌 곳을 누를 경우 비활성화
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (!_isPasswordValid()) return;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const BirthdayScreen(),
+      ),
+    );
+  }
+
+  // x 누를 시 비밀번호 삭제
+  void _onClearTap() {
+    _passwordController.clear();
+  }
+
+  void _toggleObscureText() {
+    // 아래의 과정이 두 줄로 생략된다.
+    // if (_obscureText == true) {
+    //   setState(() {
+    //     _obscureText = false;
+    //   });
+    // } else {
+    //   setState(() {
+    //     _obscureText = true;
+    //   });
+    // }
+    _obscureText = !_obscureText;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Sign up",
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                "Password",
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                controller: _passwordController,
+                onEditingComplete: _onSubmit,
+                // 비밀번호 보이지 않게
+                obscureText: _obscureText,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  suffix: Row(
+                    // 기본으로 두면 Row는 가질 수 있는 최대의 길이를 가진다
+                    // - 아이콘이 맨 앞에 배치됨... suffix인데도!
+                    // 이를 방지하기 위해 사이즈를 최소로 설정한다.
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      GestureDetector(
+                        onTap: _onClearTap,
+                        child: FaIcon(
+                          FontAwesomeIcons.solidCircleXmark,
+                          color: Colors.grey.shade500,
+                          size: Sizes.size20,
+                        ),
+                      ),
+                      Gaps.h16,
+                      GestureDetector(
+                        onTap: _toggleObscureText,
+                        child: FaIcon(
+                          _obscureText
+                              ? FontAwesomeIcons.eyeSlash
+                              : FontAwesomeIcons.eye,
+                          color: Colors.grey.shade500,
+                          size: Sizes.size20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  hintText: "Make it Strong!",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v10,
+              const Text(
+                "Your password must have:",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Gaps.v10,
+              Row(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.circleCheck,
+                    size: Sizes.size20,
+                    color: _isPasswordValid() ? Colors.green : Colors.grey,
+                  ),
+                  Gaps.h5,
+                  const Text(
+                    "8 to 20 characters",
+                  ),
+                ],
+              ),
+              Gaps.v20,
+              GestureDetector(
+                onTap: _onSubmit,
+                child: FormButton(
+                  disabled: !_isPasswordValid(),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
