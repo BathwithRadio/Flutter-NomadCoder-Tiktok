7c14f92ef93f9e73901086654cef2a95c151ea46
유민기 <mgyou@nextree.io>
2023-02-14 21:33:00 +0800

    Birthday Screen

--- tiktok_clone/lib/features/authentication/birthday_screen.dart
+++ tiktok_clone/lib/features/authentication/birthday_screen.dart
@@ -1,10 +1,126 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interest_screen.dart';
 
-class BirthdayScreen extends StatelessWidget {
+class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({super.key});
 
   @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  // Controller - allow us control widget from code, method
+  final TextEditingController _birthdayController = TextEditingController();
+
+  DateTime initialDate = DateTime.now();
+
+  @override
+  void initState() {
+    //
+    super.initState(); // state initialize - so this coma first
+    _setTextFieldDate(initialDate);
+  }
+
+  // 반드시 dispose 할 것 - 하지 않으면 언젠가 앱이 메모리 부족으로 크래시남
+  @override
+  void dispose() {
+    _birthdayController.dispose();
+    super.dispose(); // after all - clean up - dispose at latest
+  }
+
+  //Stateful widget은 context를 받는 것이 필요 없다.
+  // inside of state, you can receive context when, whereever
+  void _onNextTap() {
+    //
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const InterestsScreen(),
+      ),
+    );
+  }
+
+  void _setTextFieldDate(DateTime initialDate) {
+    //
+    // how to control Text field value
+    final textDate = initialDate.toString().split(" ").first;
+    _birthdayController.value = TextEditingValue(text: textDate);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Sign up",
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "When's your birthday?",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            Gaps.v8,
+            const Text(
+              "Your birthday won't be shown publicly.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              enabled: false,
+              controller: _birthdayController,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v20,
+            GestureDetector(
+              //Stateful widget은 context를 받는 것이 필요 없다.
+              onTap: _onNextTap,
+              child: const FormButton(
+                disabled: false,
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 300,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.date,
+            maximumDate: initialDate,
+            initialDateTime: initialDate,
+            onDateTimeChanged: _setTextFieldDate,
+          ),
+        ),
+      ),
+    );
   }
 }
