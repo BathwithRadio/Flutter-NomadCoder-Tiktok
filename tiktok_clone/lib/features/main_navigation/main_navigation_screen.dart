b07686aef5af6dbb5d9c2a0ee97bbc9d7a5e76fd
유민기 <mgyou@nextree.io>
2023-02-19 13:57:07 +0800

    BottomNavigationBar

--- tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
+++ tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigatoinScreen extends StatelessWidget {
+class MainNavigatoinScreen extends StatefulWidget {
   const MainNavigatoinScreen({super.key});
 
   @override
+  State<MainNavigatoinScreen> createState() => _MainNavigatoinScreenState();
+}
+
+class _MainNavigatoinScreenState extends State<MainNavigatoinScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text("home"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        // selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "What are you?",
+            backgroundColor: Colors.blue,
+          ),
+        ],
+      ),
+    );
   }
 }
