13e2223da2642de9cba5b7b9ae89e483b658e2c5
유민기 <mgyou@nextree.io>
2023-02-19 15:12:16 +0800

    checking navigation problem - keep destroying our state

--- tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
+++ tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/stf_screen.dart';
 
 class MainNavigatoinScreen extends StatefulWidget {
   const MainNavigatoinScreen({super.key});
@@ -14,18 +15,43 @@ class _MainNavigatoinScreenState extends State<MainNavigatoinScreen> {
   int _selectedIndex = 0;
 
   final screens = [
-    const Center(
-      child: Text(
-        "home",
-        style: TextStyle(fontSize: 49),
-      ),
+    StfScreen(
+      key: GlobalKey(),
     ),
-    const Center(
-      child: Text(
-        "Search",
-        style: TextStyle(fontSize: 49),
-      ),
+    StfScreen(
+      key: GlobalKey(),
     ),
+    // const Center(
+    //   child: Text(
+    //     "home",
+    //     style: TextStyle(fontSize: 49),
+    //   ),
+    // ),
+    // const Center(
+    //   child: Text(
+    //     "Search",
+    //     style: TextStyle(fontSize: 49),
+    //   ),
+    // ),
+    Container(),
+    StfScreen(
+      key: GlobalKey(),
+    ),
+    StfScreen(
+      key: GlobalKey(),
+    ),
+    // const Center(
+    //   child: Text(
+    //     "InBox",
+    //     style: TextStyle(fontSize: 49),
+    //   ),
+    // ),
+    // const Center(
+    //   child: Text(
+    //     "Profile",
+    //     style: TextStyle(fontSize: 49),
+    //   ),
+    // ),
   ];
 
   void _onTap(int index) {
@@ -37,6 +63,7 @@ class _MainNavigatoinScreenState extends State<MainNavigatoinScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: screens.elementAt(_selectedIndex),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
@@ -48,24 +75,28 @@ class _MainNavigatoinScreenState extends State<MainNavigatoinScreen> {
                 text: "Home",
                 isSelected: _selectedIndex == 0,
                 icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
                 onTap: () => _onTap(0),
               ),
               NavTab(
                 text: "Discover",
                 isSelected: _selectedIndex == 1,
-                icon: FontAwesomeIcons.magnifyingGlass,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
                 onTap: () => _onTap(1),
               ),
               NavTab(
                 text: "Inbox",
                 isSelected: _selectedIndex == 3,
                 icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
                 onTap: () => _onTap(3),
               ),
               NavTab(
                 text: "Profile",
                 isSelected: _selectedIndex == 4,
                 icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
                 onTap: () => _onTap(4),
               ),
             ],
