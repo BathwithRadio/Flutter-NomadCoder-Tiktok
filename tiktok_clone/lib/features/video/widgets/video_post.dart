76515113a3769a3a3a14de532e17c3fdfb949aa2
유민기 <mgyou@nextree.io>
2023-02-28 21:53:30 +0800

    Comments

--- tiktok_clone/lib/features/video/widgets/video_post.dart
+++ tiktok_clone/lib/features/video/widgets/video_post.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/video/widgets/video_button.dart';
+import 'package:tiktok_clone/features/video/widgets/video_comments.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -79,7 +80,9 @@ class _VideoPostState extends State<VideoPost>
 
   void _onVisibilityChange(VisibilityInfo info) {
     //
-    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+    if (info.visibleFraction == 1 &&
+        !_isPaused &&
+        !_videoPlayerController.value.isPlaying) {
       _videoPlayerController.play();
     }
   }
@@ -108,6 +111,20 @@ class _VideoPostState extends State<VideoPost>
     return _inputCaption.length > 25
         ? "${_inputCaption.substring(0, 25)} ..."
         : _inputCaption;
+  }
+
+  void _onCommentsTap(BuildContext context) async {
+    if (_videoPlayerController.value.isPlaying) {
+      _onTogglePause();
+    }
+    // 내장 함수
+    // 여기 await는 모달을 닫고나면 resolve된다.
+    await showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (context) => const VideoComments(),
+    );
+    _onTogglePause();
   }
 
   @override
@@ -202,8 +219,8 @@ class _VideoPostState extends State<VideoPost>
             bottom: 20,
             right: 10,
             child: Column(
-              children: const [
-                CircleAvatar(
+              children: [
+                const CircleAvatar(
                   radius: 25,
                   backgroundColor: Colors.black87,
                   foregroundColor: Colors.white,
@@ -212,17 +229,20 @@ class _VideoPostState extends State<VideoPost>
                   child: Text("Alsrl"),
                 ),
                 Gaps.v24,
-                VideoButton(
+                const VideoButton(
                   icon: FontAwesomeIcons.solidHeart,
                   text: "2.9M",
                 ),
                 Gaps.v24,
-                VideoButton(
-                  icon: FontAwesomeIcons.solidComment,
-                  text: "33K",
+                GestureDetector(
+                  onTap: () => _onCommentsTap(context),
+                  child: const VideoButton(
+                    icon: FontAwesomeIcons.solidComment,
+                    text: "33K",
+                  ),
                 ),
                 Gaps.v24,
-                VideoButton(
+                const VideoButton(
                   icon: FontAwesomeIcons.share,
                   text: "Share",
                 ),
