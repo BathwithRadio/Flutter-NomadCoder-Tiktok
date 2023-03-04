89b48e8dcc8f11037b0caa144cd52473b4fa85be
유민기 <mgyou@nextree.io>
2023-03-04 10:57:25 +0800

    TextEditingController search-CupertinoSearchTextField

--- tiktok_clone/lib/features/discover/discover_screen.dart
+++ tiktok_clone/lib/features/discover/discover_screen.dart
@@ -1,19 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 final tabs = ['Top', 'Users', 'Videos', 'Sounds', 'LIVE', 'Shopping', 'Brands'];
 
-class DiscoverScreen extends StatelessWidget {
+class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({super.key});
+
+  @override
+  State<DiscoverScreen> createState() => _DiscoverScreenState();
+}
+
+class _DiscoverScreenState extends State<DiscoverScreen> {
+  final TextEditingController _textEditingController =
+      TextEditingController(text: "Initial Text");
+
+  void _onSearchChanged(String value) {
+    print("Searching form $value");
+  }
+
+  void _onSearchSubmitted(String value) {
+    print("Submitted value $value");
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
+        // 키보드 올라올 때 리사이즈 해제
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           elevation: 1,
-          title: const Text("Discover"),
+          title: CupertinoSearchTextField(
+            controller: _textEditingController,
+            onChanged: _onSearchChanged,
+            onSubmitted: _onSearchSubmitted,
+          ),
           bottom: TabBar(
             splashFactory: NoSplash.splashFactory,
             padding: const EdgeInsets.symmetric(
@@ -38,6 +69,7 @@ class DiscoverScreen extends StatelessWidget {
         body: TabBarView(children: [
           // builder 가 children을 양산하는 것 보다 성능이 더 좋다
           GridView.builder(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
             itemCount: 20,
             padding: const EdgeInsets.all(
               Sizes.size6,
@@ -48,21 +80,70 @@ class DiscoverScreen extends StatelessWidget {
               crossAxisCount: 2,
               crossAxisSpacing: Sizes.size10,
               mainAxisSpacing: Sizes.size10,
-              childAspectRatio: 9 / 16,
+              childAspectRatio: 9 / 20,
             ),
             // 웹에서 이미지를 가져오는동안 placeholder를 띄워준다
             itemBuilder: (context, index) => Column(
               children: [
-                AspectRatio(
-                  aspectRatio: 9 / 16,
-                  child: FadeInImage.assetNetwork(
-                    fit: BoxFit.cover,
-                    placeholderFit: BoxFit.fitHeight,
-                    placeholder: "assets/images/placeholder.jpg",
-                    image:
-                        "https://images.unsplash.com/photo-1677549254885-cf55be3e552b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+                Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      Sizes.size4,
+                    ),
+                  ),
+                  child: AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                      fit: BoxFit.cover,
+                      placeholderFit: BoxFit.fitHeight,
+                      placeholder: "assets/images/placeholder.jpg",
+                      image:
+                          "https://images.unsplash.com/photo-1677549254885-cf55be3e552b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+                    ),
                   ),
                 ),
+                Gaps.v10,
+                const Text(
+                  "This is a very long caption for my ticktok so I'm uploading no currently.",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: Sizes.size16 + Sizes.size2,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v8,
+                DefaultTextStyle(
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 12,
+                        backgroundImage: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/86900125?v=4"),
+                      ),
+                      Gaps.h4,
+                      const Expanded(
+                        child: Text(
+                          "My avatar is gonna be very long!!",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size16,
+                        color: Colors.grey.shade600,
+                      ),
+                      Gaps.h2,
+                      const Text("2.5M")
+                    ],
+                  ),
+                )
               ],
             ),
           ),
