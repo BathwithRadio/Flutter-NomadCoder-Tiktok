137e47cf6bb444184c8925f5bf08f043c4791c6c
유민기 <mgyou@nextree.io>
2023-03-07 20:37:14 +0800

    AnimatedList, Transitions

---
+++ tiktok_clone/lib/features/inbox/chats_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(milliseconds: 500),
+      );
+      _items.add(_items.length);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: const Text("Direct messages"),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const FaIcon(FontAwesomeIcons.plus),
+          ),
+        ],
+      ),
+      body: AnimatedList(
+        key: _key,
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/86900125?v=4"),
+                  child: Text("Alsrl"),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "ASDF ($index)",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    // trailing은 중앙 정렬이기 때문에 title에 정렬하기 위해 Row사용
+                    Text(
+                      "2:16PM",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: const Text("Don't Forget commit"),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
