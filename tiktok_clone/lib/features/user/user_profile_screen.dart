739f0080329352e9ed37cf178b70bf324783bb81
유민기 <mgyou@nextree.io>
2023-03-09 19:53:37 +0800

    SliverGrid

--- tiktok_clone/lib/features/user/user_profile_screen.dart
+++ tiktok_clone/lib/features/user/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -14,9 +15,8 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       // sliver에는 sliver에 맞는 위젯만 넣어야 함
       slivers: [
         SliverAppBar(
-          floating: true,
-          stretch: true,
           pinned: true,
+          stretch: true,
           backgroundColor: Colors.teal,
           elevation: 1,
           collapsedHeight: 80,
@@ -24,13 +24,45 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
           flexibleSpace: FlexibleSpaceBar(
             stretchModes: const [
               StretchMode.blurBackground,
-              StretchMode.zoomBackground
+              StretchMode.zoomBackground,
+              StretchMode.fadeTitle,
             ],
             background: Image.asset(
               "assets/images/placeholder.jpg",
               fit: BoxFit.cover,
             ),
             title: const Text("Hello"),
+          ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.blueGrey[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
+          ),
+          itemExtent: 100,
+        ),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.blue[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
+          ),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100,
+            mainAxisSpacing: Sizes.size20,
+            crossAxisSpacing: Sizes.size20,
+            childAspectRatio: 1,
           ),
         ),
       ],
