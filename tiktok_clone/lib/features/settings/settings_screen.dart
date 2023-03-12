86d21c5cfde6e6ef2e6706a3c9552cbb9c978d07
유민기 <mgyou@nextree.io>
2023-03-12 14:16:10 +0800

    AboutListTile

--- tiktok_clone/lib/features/settings/settings_screen.dart
+++ tiktok_clone/lib/features/settings/settings_screen.dart
@@ -9,28 +9,9 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: Column(
+      body: ListView(
         children: const [
-          CircularProgressIndicator.adaptive(),
-          // ListWheelScrollView(
-          //   diameterRatio: 2,
-          //   offAxisFraction: 1,
-          //   itemExtent: 200,
-          //   children: [
-          //     for (var x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 11, 1])
-          //       FractionallySizedBox(
-          //         widthFactor: 1,
-          //         child: Container(
-          //           color: Colors.teal,
-          //           alignment: Alignment.center,
-          //           child: const Text(
-          //             "Pick me",
-          //             style: TextStyle(color: Colors.white, fontSize: 39),
-          //           ),
-          //         ),
-          //       ),
-          //   ],
-          // ),
+          AboutListTile(),
         ],
       ),
     );
