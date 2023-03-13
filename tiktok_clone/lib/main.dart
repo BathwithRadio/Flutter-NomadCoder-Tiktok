e55f63a742aae781cc9f8e5b2eca511ebce08e0d
유민기 <mgyou@nextree.io>
2023-03-13 20:34:52 +0800

    How to change landscape view - Orientation

--- tiktok_clone/lib/main.dart
+++ tiktok_clone/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -13,6 +13,7 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
@@ -35,7 +36,7 @@ class TikTokApp extends StatelessWidget {
               fontWeight: FontWeight.w600,
             )),
       ),
-      home: const MainNavigatoinScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
