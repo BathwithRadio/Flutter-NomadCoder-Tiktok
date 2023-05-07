f3854a12cfa4137049bc511fcb0a1df75704b299
유민기 <mgyou@nextree.io>
2023-05-07 13:45:06 +0800

    ChangeNotifier

--- tiktok_clone/lib/common/widgets/video_configuration/video_config.dart
+++ tiktok_clone/lib/common/widgets/video_configuration/video_config.dart
@@ -1,55 +1,13 @@
 import 'package:flutter/material.dart';
 
-// inheritedWidget이 앱 안의 모든 위치에서 데이터에 접근하도록 도와줌
-class VideoConfigData extends InheritedWidget {
-  final bool autoMute;
-
-  final void Function() toggleMuted;
-
-  const VideoConfigData({
-    super.key,
-    required super.child,
-    required this.autoMute,
-    required this.toggleMuted,
-  });
-
-  // static method - of
-  static VideoConfigData of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<VideoConfigData>()!;
-  }
-
-  @override
-  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
-    return true;
-  }
-}
-
-// setState를 부를 때마다 자신을 새로고침
-// autoMute 데이터와 rebuild를 작동시킬 메소드 toggleMuted 공유
-class VideoConfig extends StatefulWidget {
-  final Widget child;
-
-  const VideoConfig({super.key, required this.child});
-
-  @override
-  State<VideoConfig> createState() => _VideoConfigState();
-}
-
-class _VideoConfigState extends State<VideoConfig> {
+class VideoConfig extends ChangeNotifier {
   bool autoMute = false;
 
-  void toggleMuted() {
-    setState(() {
-      autoMute = !autoMute;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return VideoConfigData(
-      toggleMuted: toggleMuted,
-      autoMute: autoMute,
-      child: widget.child,
-    );
+  void toggleAutoMute() {
+    autoMute = !autoMute;
+    // notify to listener there is data change
+    notifyListeners();
   }
 }
+
+final videoConfig = VideoConfig();
--- tiktok_clone/lib/features/settings/settings_screen.dart
+++ tiktok_clone/lib/features/settings/settings_screen.dart
@@ -37,18 +37,24 @@ class _SettingsScreenState extends State<SettingsScreen> {
         ),
         body: ListView(
           children: [
-            SwitchListTile.adaptive(
-              value: VideoConfigData.of(context).autoMute,
-              onChanged: (value) {
-                VideoConfigData.of(context).toggleMuted();
-              },
-              title: Text(
-                "Auto Mute",
-                textAlign: _setTextAlignByScreen(width),
-              ),
-              subtitle: Text(
-                "Videos will be muted by default.",
-                textAlign: _setTextAlignByScreen(width),
+            // ChangeNotifier를 사용할 때 AnimatedBuilder를 사용
+            // AnimatedBuilder는 애니매이션 뿐만 아니라 값 변경알림에도 사용됨
+            // 이렇게 사용하면 딱 이 부분만 rebuild 된다 - 전체를 rebuild하지 않아도 됨
+            AnimatedBuilder(
+              animation: videoConfig,
+              builder: (context, child) => SwitchListTile.adaptive(
+                value: videoConfig.autoMute,
+                onChanged: (value) {
+                  videoConfig.toggleAutoMute();
+                },
+                title: Text(
+                  "Auto Mute",
+                  textAlign: _setTextAlignByScreen(width),
+                ),
+                subtitle: Text(
+                  "Videos will be muted by default.",
+                  textAlign: _setTextAlignByScreen(width),
+                ),
               ),
             ),
             SwitchListTile.adaptive(
